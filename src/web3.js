cb9ab689bf4d8ab6dbb0b50afcb6287bb5ab9abf
Daniel Anatolie <daniel.anatolie@gmail.com>
2018-05-27 12:22:15 -0700

    Added comments

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,8 @@
 import Web3 from 'web3';
 
 /*
-    Extract Metamask's injected Web3 to have access to it's Rinkeby Test Network
+    Extract Metamask's injected Web3 to have access to it's Rinkeby Test Network.
+    Exporting default web3 will allow the project's version of web3 to be used instead of Metamasks's.
  */
 const web3 = new Web3(window.web3.currentProvider);
 
